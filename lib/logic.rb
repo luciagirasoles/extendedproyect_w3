bfeca9f8f83331faabe0e8ce23a4bea7af8155a6
Brayan Ciudad <linzeur@gmail.com>
2019-03-16 09:18:41 -0500

    Extended Project V1

--- lib/logic.rb
+++ lib/logic.rb
@@ -1,97 +1,6 @@
 # Esta linea está llamando a la clase Socket que tiene Ruby
 require 'socket'
-
-# html = "
-# <!DOCTYPE html>
-# <html lang=\"en\">
-
-# <head>
-#     <meta charset=\"UTF-8\">
-#     <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">
-#     <meta http-equiv=\"X-UA-Compatible\" content=\"ie=edge\">
-#     <link rel=\"stylesheet\" href=\"../first_css.css\">
-#     <title>Grupo Werewolves</title>
-# </head>
-
-# <body>
-#     <!-- <header></header> -->
-#     <main>
-#         <section class=\"principal\">
-#             <h1>Grupo Werewolves</h1>
-#         </section>
-#         <section class=\"descripcion\">
-#             <h2>Integrantes:</h2>
-#             <ul class=\"listaDetalle\">
-#                 <li class=\"detalle\">Product Owner: <a href=\"#\">Carlos Ayala</a></li>
-#                 <li class=\"detalle\">Representante Técnico: <a href=\"#\">Mayra Navarro</a></li>
-#                 <li class=\"detalle\"><a href=\"#\">Brayan Ciudad</a></li>
-#                 <li class=\"detalle\"><a href=\"#\">Jonathan Mendoza</a></li>
-#                 <li class=\"detalle\"><a href=\"#\">Christopher Roa</a></li>
-#             </ul>
-#         </section>
-#     </main>
-#     <footer>
-#         <p>&copy;2019 Grupo Werewolf</p>
-#     </footer>
-# </body>
-
-# </html>
-
-# "
-
-# css = "
-# body {
-#   margin: 0;
-#   padding: 0;
-#   background-color: #000;
-# }
-
-# main {
-#   display: flex;
-#   justify-content: center;
-#   flex-flow: column;
-#   width: 80%;
-#   margin: 0 auto;
-#   background-color: chocolate;
-# }
-
-# section {
-# }
-
-# .principal {
-#   display: flex;
-#   align-items: center;
-#   justify-content: center;
-#   height: 50vh;
-#   background-color: palegoldenrod;
-# }
-
-# .principal h1 {
-#   padding: 0;
-#   margin: 0;
-#   font-weight: bold;
-#   font-size: 4em;
-#   text-align: center;
-#   vertical-align: center;
-# }
-
-# .descripcion {
-#   height: 45vh;
-# }
-
-# footer {
-#   height: 5vh;
-#   background-color: brown;
-#   text-align: center;
-#   line-height: 2em;
-# }
-
-# footer p {
-#   padding: 0;
-#   margin: 0;
-# }
-
-# "
+require_relative 'logic_methods'
 
 # Aquí está iniciando una conexión en el puerto 8000
 server = TCPServer.new 8000
@@ -100,25 +9,38 @@ loop do
   # Espera a que un cliente se conecte
   client = server.accept
   # Recuperando toda la información enviada en la petición (request)
-  request = client.gets.gsub(/ HTTP\/1.[12]\r\n|GET \//,"")
+  req = client.gets
+  request = req.gsub(/ HTTP\/1.[12]\r\n|GET \//,"")
   result = ""
-  code_http = "404"
+  code_http = "200"
   content_type = "text/html"
-  if request.include?(".css")
-    #validar
-    code_http = "200"
-    content_type = "text/css"
-  else
-    if request == ""
-    elsif request.include?("?member=")
-
-      code_http = "200"
+  if request.include?("?member=")
+    request["?member="] = ""
+    if check_file_existence(request)
+      data = read_string(request + ".txt").split("---------")
+      result = read_string("about.html")
+      7.times { |val| result["YYYYYYYY#{val + 1}"] = data[val]; }
+    else
+      code_http = "404"
+      result = read_string("error.html")
     end
+  elsif /.css|.ico/ === request || request == ""
+    if request.include?(".css")
+      content_type = "text/css"
+    elsif request.include?(".ico")
+      content_type = "image/x-icon"
+    elsif request == ""
+      request = "index.html"
+    end
+    result = read_string(request)
+  else
+    code_http = "400"
+    request = "error.html" unless request == "error.html"
+    result = read_string(request)
   end
-
   client.print "HTTP/1.1 #{code_http}\n"
-  client.print "Content-Type: #{content_type}\n" # 2
-  client.print "\n" # 3
+  client.print "Content-Type: #{content_type}\n"
+  client.print "\n"
   client.print result
   client.close
 end
--- lib/logic_methods.rb
+++ lib/logic_methods.rb
@@ -1,29 +1,24 @@
 #validar si del nombre ingresado existe el .txt
 def check_file_existence(name)
-    @name = '../Data/' + name.to_s.capitalize + '.txt'
-    File.exist?(@name) || File.symlink?(@name) 
+    name = '../data/' + name.capitalize + '.txt'
+    File.exist?(name) || File.symlink?(name) 
 end
 
-# metodo recibe string (nombre o error o vacio)
+# metodo recibe string
 # debe leer los archivos o cargar la pagina de error
-
-# a = [ "a", "b", "c" ]
-# a.include?("b")   #=> true
-# a.include?("z")   #=> false
 
 def read_string(string_name) 
     string_result = ""
 
     if string_name.include? ".txt"
-        path_string = "../Data/#{string_name}"
-    elsif string_name.include? "error.html"
-        path_string = "../public/#{string_name}"
-    elsif string_name.include? ".css"
+        path_string = "../data/#{string_name.capitalize}"
+    elsif string_name.include? ".ico"
         path_string = "../styles/#{string_name}"
-    elsif
+    elsif string_name == "error.html"
+        path_string = "../public/#{string_name}"
+    else
         path_string = "../#{string_name}"
     end
-
     path = Dir.glob(path_string).first
     File.open(path) do |f|
         f.each do |line|
